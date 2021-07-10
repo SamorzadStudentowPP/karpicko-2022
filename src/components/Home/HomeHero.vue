cf7976a3c0160f68fde64f7de672c2a49607aa70
mozzar <realmozzar@gmail.com>
2021-07-10 22:01:16 +0200

    pierwsze zmiany

--- src/components/Home/HomeHero.vue
+++ src/components/Home/HomeHero.vue
@@ -4,14 +4,14 @@
       <v-row align="center" justify="center" class="mt-n12">
         <v-col class="text-center" cols="12">
           <v-img
-            src="@/assets/other/Karpicko_White_Logo_2020.png"
+            src="@/assets/other/Karpicko_White_Logo_2021.png"
             contain
             max-height="300"
           ></v-img>
           <h2 class="display-0 font-weight-thin mb-4">
             Obóz Integracyjno - Szkoleniowy Karpicko 2021
           </h2>
-          <h3 class="subheading">Zapisy</h3>
+          <h3 class="subheading">Zapisy wkrótce!</h3>
         </v-col>
       </v-row>
     </v-parallax>
